0f84e8e738ff82875f91db79518e6b6149bd3cf8
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-06-07 21:18:26 -0500

    Event handler for interacting with all number buttons in place and working

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import './App.css';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
-import { addOne } from './actions'
+// import { addOne } from './actions'
+import { applyNumber } from './actions'
 
 function App() {
 
 	const [state, dispatch] = useReducer(reducer, initialState);
 
-	const eventHandler = (e) => {
-		dispatch(addOne())
+	// const eventHandler = () => {
+	// 	dispatch(addOne())
+	// }
+
+	const doMath = (number) => {
+		dispatch(applyNumber(number))
 	}
 
   return (
@@ -39,21 +44,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={eventHandler} value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={() => doMath(1)} value={1}/>
+              <CalcButton onClick={() => doMath(2)} value={2}/>
+              <CalcButton onClick={() => doMath(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={() => doMath(4)} value={4}/>
+              <CalcButton onClick={() => doMath(5)} value={5}/>
+              <CalcButton onClick={() => doMath(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={() => doMath(7)} value={7}/>
+              <CalcButton onClick={() => doMath(8)} value={8}/>
+              <CalcButton onClick={() => doMath(9)} value={9}/>
             </div>
 
             <div className="row">
